ffa38efe2c7456890db36ee6c5fdc3c11a538ce3
khaildalhadil <khaild.alhadi2020@gamil.com>
2026-03-17 13:29:45 +0400

    add categoires component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,6 +3,7 @@
 import { Link } from "@/i18n/navigation";
 import Image from "next/image";
 import { useTranslations } from "use-intl";
+import Categories from "../components/Categories";
 
 
 export default function Home() {
@@ -10,101 +11,105 @@ export default function Home() {
   const t = useTranslations('HomePage')
 
   return (
-    <div className="flex flex-col lg:flex-row items-center justify-between gap-5 mt-20 mb-30">
-      <div className="flex-1">
-        <h1 className="text-5xl font-bold mb-5">{t("title")}</h1>
-        <p className="text-lg">{t("description")}</p>
-        <div>
-          <div className="flex gap-4">
-            <Link
-              href={"/planner"}
-              className="bg-green-600 text-white px-4 py-2 rounded mt-5 text-lg cursor-pointer"
-            >
-              {t("start_now")}
-            </Link>
-            <Link
-              href={"/destinations"}
-              className=" border border-gray-400 px-4 py-2 rounded mt-5 text-lg cursor-pointer"
-            >
-              {t("learn_more")}
-            </Link>
-          </div>
-
-          {/* i should be dry here  */}
-          <div className="flex items-center gap-5">
-            <div className="flex items-center my-5 relative">
-              <Image
-                src="https://img.freepik.com/free-photo/happy-man-student-with-afro-hairdo-shows-white-teeth-being-good-mood-after-classes_273609-16608.jpg"
-                alt="oman"
-                width={40}
-                height={40}
-                className=" rounded-full h-10 w-10 object-cover"
-                placeholder="blur"
-                blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
-              />
-              <Image
-                src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?semt=ais_rp_progressive&w=740&q=80"
-                alt="oman"
-                width={40}
-                height={40}
-                className=" rounded-full h-10 w-10 object-cover"
-                placeholder="blur"
-                blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
-              />
-              <Image
-                src="https://img.freepik.com/free-photo/serious-young-african-man-standing-isolated_171337-9633.jpg?semt=ais_hybrid&w=740&q=80"
-                alt="oman"
-                width={40}
-                height={40}
-                className=" rounded-full h-10 w-10 object-cover "
-                placeholder="blur"
-                blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
-              />
-              <Image
-                src="https://img.freepik.com/photos-gratuite/smiley-bel-homme-posant_23-2148911841.jpg?semt=ais_hybrid&w=740&q=80"
-                alt="oman"
-                width={40}
-                height={40}
-                className=" rounded-full h-10 w-10 object-cover "
-                placeholder="blur"
-                blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
-              />
+    <div className="flex flex-col gap-5">
+      <div className="flex flex-col lg:flex-row items-center justify-between gap-5 mt-20 mb-30">
+        <div className="flex-1">
+          <h1 className="text-5xl font-bold mb-5">{t("title")}</h1>
+          <p className="text-lg">{t("description")}</p>
+          <div>
+            <div className="flex gap-4">
+              <Link
+                href={"/planner"}
+                className="bg-green-600 text-white px-4 py-2 rounded mt-5 text-lg cursor-pointer"
+              >
+                {t("start_now")}
+              </Link>
+              <Link
+                href={"/destinations"}
+                className=" border border-gray-400 px-4 py-2 rounded mt-5 text-lg cursor-pointer"
+              >
+                {t("learn_more")}
+              </Link>
             </div>
 
-            <p className="font-bold">{t("visitors")}</p>
+            {/* i should be dry here  */}
+            <div className="flex items-center gap-5">
+              <div className="flex items-center my-5 relative">
+                <Image
+                  src="https://img.freepik.com/free-photo/happy-man-student-with-afro-hairdo-shows-white-teeth-being-good-mood-after-classes_273609-16608.jpg"
+                  alt="oman"
+                  width={40}
+                  height={40}
+                  className=" rounded-full h-10 w-10 object-cover"
+                  placeholder="blur"
+                  blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
+                />
+                <Image
+                  src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?semt=ais_rp_progressive&w=740&q=80"
+                  alt="oman"
+                  width={40}
+                  height={40}
+                  className=" rounded-full h-10 w-10 object-cover"
+                  placeholder="blur"
+                  blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
+                />
+                <Image
+                  src="https://img.freepik.com/free-photo/serious-young-african-man-standing-isolated_171337-9633.jpg?semt=ais_hybrid&w=740&q=80"
+                  alt="oman"
+                  width={40}
+                  height={40}
+                  className=" rounded-full h-10 w-10 object-cover "
+                  placeholder="blur"
+                  blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
+                />
+                <Image
+                  src="https://img.freepik.com/photos-gratuite/smiley-bel-homme-posant_23-2148911841.jpg?semt=ais_hybrid&w=740&q=80"
+                  alt="oman"
+                  width={40}
+                  height={40}
+                  className=" rounded-full h-10 w-10 object-cover "
+                  placeholder="blur"
+                  blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
+                />
+              </div>
+
+              <p className="font-bold">{t("visitors")}</p>
+            </div>
           </div>
+        </div>
+
+        <div className="flex-1 relative h-96 mt-16">
+          <Image
+            src="/images/hero_1.jpg"
+            alt="oman"
+            width={400}
+            height={400}
+            className="top-0 right-1/3 hero_ima z-10"
+            placeholder="blur"
+            blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
+          />
+          <Image
+            src="/images/hero_2.jpg"
+            alt="oman"
+            width={400}
+            height={400}
+            className="top-10 left-3/9 hero_ima"
+            placeholder="blur"
+            blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
+          />
+          <Image
+            src="/images/hero_3.jpg"
+            alt="oman"
+            width={400}
+            height={400}
+            className="hero_ima top-50 right-1/5"
+            placeholder="blur"
+            blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
+          />
         </div>
       </div>
 
-      <div className="flex-1 relative h-96 mt-16">
-        <Image
-          src="/images/hero_1.jpg"
-          alt="oman"
-          width={400}
-          height={400}
-          className="top-0 right-1/3 hero_ima z-10"
-          placeholder="blur"
-          blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
-        />
-        <Image
-          src="/images/hero_2.jpg"
-          alt="oman"
-          width={400}
-          height={400}
-          className="top-10 left-3/9 hero_ima"
-          placeholder="blur"
-          blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
-        />
-        <Image
-          src="/images/hero_3.jpg"
-          alt="oman"
-          width={400}
-          height={400}
-          className="hero_ima top-50 right-1/5"
-          placeholder="blur"
-          blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
-        />
-      </div>
+      <Categories />
     </div>
   );
 }
---
+++ app/components/Categories.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image"
+import Link from "next/link"
+
+// i should convert them from mb to kb to make them small size
+const categories = [
+  {name: "Culture", image: "Culture.jpg"},
+  {name: "Food", image: "Food.jpg"},
+  {name: "Beach", image: "Beach.jpg"},
+  {name: "Nature", image: "Nature.jpg"},
+  {name: "Mountain", image: "Mountain.jpg"},
+  {name: "Desert", image: "Desert.jpg"},
+]
+
+export default function Categories() {
+  return (
+    <div className="border-t border-neutral-300 pt-10 ">
+      <h1 className="text-2xl font-bold mb-10">
+        Categories
+      </h1>
+      <div>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-[1fr_1fr_1fr] gap-5">
+          {categories.map((c, i) => 
+            <li key={i} className=" relative">
+              <Link href={`/destinations?page=1&per_page=20&region=all&season=all&category=${c.name}`}>
+                <Image
+                  src={`/images/${c.image}`}
+                  alt="oman"
+                  width={100}
+                  height={100}
+                  className="w-full h-full rounded"
+                  placeholder="blur"
+                  blurDataURL="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgdmlld0JveD0iMCAwIDQwMCA0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIGZpbGw9IiNlMGUwZTAiIC8+PC9zdmc+"
+                />
+                <p className=" absolute bottom-0 left-1/2 -translate-x-1/2 font-bold text-white text-2xl backdrop-blur-2xl w-full text-center ">{c.name}</p>
+              </Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  )
+}
